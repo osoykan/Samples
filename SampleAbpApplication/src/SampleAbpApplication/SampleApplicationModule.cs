7fea533c5dd4cb6fe85d980fd75a6c7f26098a2b
osoykan <oguzhansoykan@gmail.com>
2017-03-06 22:38:11 +0300

    new features tried

--- SampleAbpApplication/src/SampleAbpApplication/SampleApplicationModule.cs
+++ SampleAbpApplication/src/SampleAbpApplication/SampleApplicationModule.cs
@@ -1,25 +1,19 @@
 ﻿using System.Reflection;
 
 using Abp;
-using Abp.Configuration.Startup;
-using Abp.Dependency;
+using Abp.Dapper;
 using Abp.EntityFramework;
-using Abp.EntityFramework.Uow;
 using Abp.Modules;
 
 namespace SampleAbpApplication
 {
     [DependsOn(
         typeof(AbpEntityFrameworkModule),
-        typeof(AbpKernelModule)
+        typeof(AbpKernelModule),
+        typeof(AbpDapperModule)
     )]
     public class SampleApplicationModule : AbpModule
     {
-        public override void PreInitialize()
-        {
-            Configuration.ReplaceService<IEfTransactionStrategy, DbContextEfTransactionStrategy>(DependencyLifeStyle.Transient);
-        }
-
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
