f38c00b2fef93163c6271ca43546f8ecff634768
osoykan <oguzhansoykan@gmail.com>
2017-03-06 22:38:11 +0300

    new features tried

--- SampleAbpApplication/src/SampleAbpApplication/SomeDomainService.cs
+++ SampleAbpApplication/src/SampleAbpApplication/SomeDomainService.cs
@@ -1,7 +1,11 @@
-﻿using Abp.Dependency;
+﻿using System.Linq;
+
+using Abp.Dapper.Dapper.Repositories;
+using Abp.Dependency;
 using Abp.Domain.Repositories;
 using Abp.Domain.Uow;
 using Abp.EntityFramework;
+using Abp.EntityFramework.Extensions;
 
 using SampleAbpApplication.DbContexes;
 using SampleAbpApplication.Entities;
@@ -12,6 +16,7 @@ namespace SampleAbpApplication
     {
         private readonly IDbContextProvider<AnimalDbContext> _animalDbContextProvider;
         private readonly IRepository<Animal> _animalRepository;
+        private readonly IDapperRepository<Person> _personDapperRepository;
         private readonly IRepository<Person> _personRepository;
         private readonly IUnitOfWorkManager _unitOfWorkManager;
 
@@ -19,12 +24,14 @@ namespace SampleAbpApplication
             IUnitOfWorkManager unitOfWorkManager,
             IRepository<Person> personRepository,
             IRepository<Animal> animalRepository,
-            IDbContextProvider<AnimalDbContext> animalDbContextProvider)
+            IDbContextProvider<AnimalDbContext> animalDbContextProvider,
+            IDapperRepository<Person> personDapperRepository)
         {
             _unitOfWorkManager = unitOfWorkManager;
             _personRepository = personRepository;
             _animalRepository = animalRepository;
             _animalDbContextProvider = animalDbContextProvider;
+            _personDapperRepository = personDapperRepository;
         }
 
         public void DoSomeStuff()
@@ -42,6 +49,10 @@ namespace SampleAbpApplication
                 _unitOfWorkManager.Current.SaveChanges();
 
                 Animal animal = _animalRepository.FirstOrDefault(x => x.Name == "Kuş");
+
+                Person person = _personDapperRepository.Get(1);
+
+                Person anotherPerson = _personRepository.Nolocking(persons => persons.FirstOrDefault(x => x.Name == "Ekmek"));
 
                 uow.Complete();
             }
